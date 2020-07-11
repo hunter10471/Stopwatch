d5bc9283ad3e29349a3d393c03138960a32fcc05
Rafay <64031723+hunter10471@users.noreply.github.com>
2020-07-11 21:41:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+var min = 0;
+var sec = 0;
+var msec = 0;
+
+var minheading = document.getElementById("min");
+var secheading = document.getElementById("sec");
+var msecheading = document.getElementById("msec");
+
+var interval ;
+function timer (){
+    msec++;
+    msecheading.innerHTML = msec;
+    if (msec >= 100){
+        sec++;
+        secheading.innerHTML = sec
+        msec = 0
+    }
+    else if (sec>=60){
+        min++;
+        minheading.innerHTML = min;
+        sec = 0
+    }
+
+}
+ 
+function start(){
+interval = setInterval(timer,10)
+disableButton()
+}
+function stop(){
+    clearInterval(interval)
+}
+function reset(){
+    min = 0
+    sec = 0
+    msec = 0
+    minheading.innerHTML = min
+    secheading.innerHTML = sec
+    msecheading.innerHTML = msec
+    stop()
+    enablebutton()
+}
+function disableButton() {
+    var start = document.getElementById('start');
+    start.disabled = true;
+}
+function enablebutton(){
+    var start = document.getElementById("start")
+    start.disabled = false;
+}
